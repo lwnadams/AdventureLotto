2c5e07b699d8a198cb029cb984efccc7115c1df8
Lawrence Adams <larrymusic2004@hotmail.com>
2024-09-13 20:40:31 +0100

    Added PC random number guessing with colors and fase

--- js/script.js
+++ js/script.js
@@ -18,15 +18,14 @@ function drawSquares(x){
 
 
 
-    divlottery.innerHTML = '<div id="#0", class="play-game-random-digit"></div><div id="#1", class="play-game-random-digit"></div>'
+    divlottery.innerHTML = '<div id="o0", class="play-game-random-digit"></div><div id="o1", class="play-game-random-digit"></div>'
     divinput.innerHTML = '<input type="number" id="i0", class="play-game-inputs", name="quantity" min="0" max="9"><input type="number" id="i1", class="play-game-inputs", name="quantity" min="0" max="9">'
     
     for (let i = 2; i <= x+2; i++){
 
         let newDigitOut = document.createElement("div");
         newDigitOut.classList.add('play-game-random-digit')
-        newDigitOut.id = '#'+i
-        newDigitOut.textContent = 'test '+i;
+        newDigitOut.id = 'o'+i
         divlottery.appendChild(newDigitOut)
 
         let newDigitin = document.createElement("input");
@@ -52,6 +51,26 @@ function myFunc(key){
         }
     }
 }
+
+function django(count){
+    pcNumbers = []
+    for (let i = 0; i < count; i++){
+        computerNumber = Math.floor(Math.random() * 10)
+        pcNumbers.push(computerNumber)
+    }
+    return pcNumbers
+}
+
+function compareNumbers(userNumbers, pcNumbers){
+        for (let i = 0; i < userNumbers.length; i++){
+            let outBox = document.querySelector('#o'+i)
+            outBox.innerHTML = '<h1>'+pcNumbers[i]+'</h1>'
+            outBox.style.color = 'rgb('+Math.floor(Math.random()*255)+', '+Math.floor(Math.random()*255)+', '+Math.floor(Math.random()*255)+')'
+        }
+}
+
+
+function userVictory(){}
 
 
 let divlottery = document.querySelector('.play-game-lottery-container');
@@ -86,7 +105,19 @@ divinput.addEventListener("keydown", function(key) {
 const form = document.querySelector('form'); 
 form.addEventListener('submit', function(event) { 
   event.preventDefault(); 
-  alert("You've submitted")
-  // Your form submission logic here 
+  let count = document.querySelectorAll('.play-game-inputs').length
+  let userNumbers = []
+  for (let i = 0; i < count; i ++){
+    id = '#'+'i'+i
+    let currentBox = document.querySelector(id).value
+    if (currentBox === ''){
+        alert("Whoops! Please enter a number!")
+        return
+    } else{
+        userNumbers.push(currentBox)
+    }
+  }
+  pcNumbers = django(count)
+  compareNumbers(userNumbers, pcNumbers)
 }); 
 
